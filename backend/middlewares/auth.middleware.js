0ff33eb69fc47f63f184b287f8564b06800fefb0
Sajjad Ali <sajadalisk958@gmail.com>
2024-12-18 15:00:25 +0500

    first commit

---
+++ backend/middlewares/auth.middleware.js
@@ -0,0 +1,5 @@
+// import jwt from "jsonwebtoken";
+
+// const isAuthenticated = async(req, res, next) => {
+//     const token = 
+// }
---
+++ backend/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    date_of_birth: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, {timestamps: true});
+
+export const User = mongoose.model("users", userSchema);
