2f7b0e71453990acd656538ca2769f9229b5e6b1
Sajjad Ali <sajadalisk958@gmail.com>
2025-01-08 14:55:00 +0500

    updating code

---
+++ models/product.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    productImages: {
+        type: [String],
+        required: true
+    }
+}, {timestamps: true});
+
+export const Product = mongoose.model("products", productSchema);
