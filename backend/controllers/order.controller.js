748cfa8933dbda2b91afbcc6a752af3815b96915
Sajjad Ali <sajadalisk958@gmail.com>
2025-01-11 19:24:36 +0500

    updated

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -69,7 +69,13 @@ export const fetchMyOrders = async (req, res) => {
 export const deleteOrderFromCart = async (req, res) => {
     const {id} = req.params;
     try {
+        const order = await Order.findByIdAndDelete(id);
+
+        if(!order) {
+            return res.status(400).json({message: "order not found"})
+        }
         
+        return res.status(200).json({message: "order deleted", order});
     } catch (error) {
         return res.status(500).json({message: "Internal server error", error})
     }
--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,16 +1,17 @@
 import express from "express";
 import isAuthenticated from "../middlewares/auth.middleware.js";
 import hasRole from "../middlewares/checkRole.middleware.js";
-import { fetchAllOrdersAdmin, fetchMyOrders, placeOrder, updateOrderStatus } from "../controllers/order.controller.js";
+import { deleteOrderFromCart, fetchAllOrdersAdmin, fetchMyOrders, placeOrder, updateOrderStatus } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 // user
 router.post("/place-order", isAuthenticated, hasRole(["user"]), placeOrder);
 router.get("/get-order", isAuthenticated, hasRole(["user"]), fetchMyOrders);
+router.delete("/delete-order/:id", isAuthenticated, hasRole(["user"]), deleteOrderFromCart);
 
  // admin
-router.get("/get-all-order", isAuthenticated, hasRole(["admin"]), fetchAllOrdersAdmin);
+router.get("/get-all-order", isAuthenticated, hasRole(["user"]), fetchAllOrdersAdmin);
 router.patch("/update-order/:id", isAuthenticated, hasRole(["admin"]), updateOrderStatus);
 
 export default router;
