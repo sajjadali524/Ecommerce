50901023195fd1dd40c2f9fb028450966529ef32
Sajjad Ali <sajadalisk958@gmail.com>
2025-01-15 16:11:59 +0500

    footer done

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom";
+import { footerLinks } from "./footerLinks";
+
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="grid lg:grid-cols-2 gap-10 w-full lg:px-32 md:px-32 px-3 py-10">
+      <div className="flex flex-col w-[80%]">
+         <Link to="/" className="font-[500] text-pink-600">
+          ECO<sapn className="text-black">MMERCE</sapn>
+        </Link>
+        <p>Lorem, ipsum dolor sit doloribus asperiores rerum corrupti suscipit sit, doloremque fuga esse voluptate blanditiis harum sint sed voluptatibus quibusdam architecto.</p>
+      </div>
+
+      <div className="flex gap-32">
+      <div className="flex flex-col">
+        <h1>COMPANY</h1>
+        {
+          footerLinks.map((link, index) => {
+            return(
+              <Link key={index} to={link.path}>{link.title}</Link>
+            )
+          })
+        }
+      </div>
+
+      <div className="flex flex-col">
+        <h1>GET IN TOUCH</h1>
+        <p>+923483490880</p>
+        <p>engr.sajjadali2001@gmail.com</p>
+      </div>
+      </div>
     </div>
   )
 }
---
+++ frontend/src/components/Footer/footerLinks.js
@@ -0,0 +1,18 @@
+export const footerLinks = [
+    {
+        title: "Home",
+        path: "/"
+    },
+    {
+        title: "About Us",
+        path: "/about-us"
+    },
+    {
+        title: "Delivery",
+        path: "/delivery"
+    },
+    {
+        title: "Privacy Ploicy",
+        path: "/privacy-policy"
+    },
+];
